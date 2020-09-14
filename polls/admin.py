315fb42e1620b0901fd2050f43a4e6817a60abde
Imon Hossain <imon.enroute@gmail.com>
2020-09-14 11:03:58 +0600

    department filter relavent field

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from .models import School, Subject, ClassInfo, Department, AcademicYear
+from django.urls import reverse
+from django.utils.http import urlencode
 # Register your models here.
 class DepartmentModel(admin.ModelAdmin):
     list_display = ('name', 'year', 'code')
+    # list_filter = ("year", admin.RelatedOnlyFieldListFilter)
+    list_filter=[('year', admin.RelatedOnlyFieldListFilter)]
 
 class SubjectModel(admin.ModelAdmin):
     list_display = ('name', 'year', 'code', 'credit', 'marks')
